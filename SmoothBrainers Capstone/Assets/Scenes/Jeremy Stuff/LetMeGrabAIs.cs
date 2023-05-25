5dfe117bc6e93c2bd646cb697c9c7f631a56d911
CubeOf10 <cubeof10@gmail.com>
2023-05-25 15:28:30 +1000

    Adding new UI to units

--- SmoothBrainers Capstone/Assets/Scenes/Jeremy Stuff/LetMeGrabAIs.cs
+++ SmoothBrainers Capstone/Assets/Scenes/Jeremy Stuff/LetMeGrabAIs.cs
@@ -7,12 +7,14 @@ public class LetMeGrabAIs : MonoBehaviour
     OVRGrabbable grabScript;
     Tank tankScript;
     NavMeshAgent agentComp;
+    GameObject unitMenu;
     // Start is called before the first frame update
     void Start()
     {
         agentComp = gameObject.GetComponent<NavMeshAgent>();
         tankScript = gameObject.GetComponent<Tank>();
         grabScript = gameObject.GetComponent<OVRGrabbable>();
+        unitMenu = tankScript.unitDisplay;
     }
 
     // Update is called once per frame
@@ -20,10 +22,14 @@ public class LetMeGrabAIs : MonoBehaviour
     {
         agentComp.enabled = false;
         agentComp.enabled = false;
+
+        unitMenu.SetActive(true);
     }
     public void Dropped()
     {
         agentComp.enabled = true;
         tankScript.enabled = true;
+    
+        unitMenu.SetActive(false);
     }
 }
