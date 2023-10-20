3fc882aa218c68eaf7b6bc8c0d7f42539d96f66c
kylelim1028 <kylelim1028@gmail.com>
2023-10-20 23:25:57 +1000

    Comments Pen.cs

--- SmoothBrainers Capstone/Assets/Scenes/Kyle/Scripts/Pen.cs
+++ SmoothBrainers Capstone/Assets/Scenes/Kyle/Scripts/Pen.cs
@@ -11,20 +11,26 @@ public class Pen : MonoBehaviour
     public Transform tip;
     public Material drawingMaterial;
     public Material tipMaterial;
+
+    // Width Properties
     [Range(0.01f, 0.1f)]
     public float penWidth = 0.01f;
     float[] penWidths = { 0.01f, 0.02f, 0.04f };
     int currentWidthIndex = 0;
+
+    // Colour Properties
     public Color[] penColors;
+    private int currentColorIndex;
 
     List<LineRenderer> drawings = new List<LineRenderer>();
     private LineRenderer currentDrawing;
     private int index;
-    private int currentColorIndex;
 
     private bool drawing = false;
     private bool showDrawing = true;
     private bool startDrawing = false;
+
+
 
     private void Start()
     {
@@ -44,31 +50,6 @@ public class Pen : MonoBehaviour
         {
             currentDrawing = null;
         }
-
-        //if (Input.GetKeyDown(KeyCode.C))
-        //{
-        //    ClearAll();
-        //}
-        //else if (Input.GetKeyDown(KeyCode.U))
-        //{
-        //    UndoAction();
-        //}
-        //else if (Input.GetKeyDown(KeyCode.O))
-        //{
-        //    ShowDrawing();
-        //}
-        //else if (Input.GetKeyDown(KeyCode.P))
-        //{
-        //    HideDrawing();
-        //}
-        //else if (Input.GetKeyDown(KeyCode.T))
-        //{
-        //    SwitchColor();
-        //}
-        //else if (Input.GetKeyDown(KeyCode.W))
-        //{
-        //    ChangeWidth();
-        //}
     }
 
     private void Draw()
@@ -77,16 +58,20 @@ public class Pen : MonoBehaviour
         {
             index = 0;
             currentDrawing = new GameObject().AddComponent<LineRenderer>();
+
+            // Setting properties
             currentDrawing.material = drawingMaterial;
             currentDrawing.startColor = currentDrawing.endColor = penColors[currentColorIndex];
             currentDrawing.startWidth = currentDrawing.endWidth = penWidth;
             currentDrawing.positionCount = 1;
             currentDrawing.SetPosition(0, tip.position);
 
+            // Add the current drawing to the array
             drawings.Add(currentDrawing);
         }
         else
         {
+            // The current drawing
             var currentPos = currentDrawing.GetPosition(index);
             if (Vector3.Distance(currentPos, tip.position) > 0f)
             {
@@ -97,8 +82,10 @@ public class Pen : MonoBehaviour
         }
     }
 
+    // Change the colour of the pen
     public void SwitchColor()
     {
+        // Loops through an array of available colours
         if (currentColorIndex == penColors.Length - 1)
         {
             currentColorIndex = 0;
@@ -110,12 +97,15 @@ public class Pen : MonoBehaviour
         tipMaterial.color = penColors[currentColorIndex];
     }
 
+    // Change the width of the pen
     public void ChangeWidth()
     {
+        // Loops through an array of set widths
         currentWidthIndex = (currentWidthIndex + 1) % penWidths.Length;
         penWidth = penWidths[currentWidthIndex];
     }
 
+    // Clears all of the drawings in the scene
     public void ClearAll()
     {
         if (drawings.Count  > 0)
@@ -128,6 +118,7 @@ public class Pen : MonoBehaviour
         }
     }
 
+    // Undo the last drawing
     public void UndoAction()
     {
         if (drawings.Count > 0)
@@ -138,10 +129,12 @@ public class Pen : MonoBehaviour
         }
     }
 
+    // Show or Hide all drawings
     public void ShowHideDrawing()
     {
         if (drawings.Count > 0)
         {
+            // Hide Drawings
             if (showDrawing)
             {
                 foreach (var drawing in drawings)
@@ -150,6 +143,8 @@ public class Pen : MonoBehaviour
                 }
                 showDrawing = false;
             }
+
+            // Show Drawings
             else
             {
                 foreach (var drawing in drawings)
@@ -161,6 +156,7 @@ public class Pen : MonoBehaviour
         }
     }
 
+    // Start or Stop drawing
     public void StartStopDrawing()
     {
         if (!startDrawing)
@@ -173,15 +169,16 @@ public class Pen : MonoBehaviour
         }
     }
 
-
     private void OnTriggerStay(Collider other)
     {
+        // When the user is "pinching" (Colliders on the Index and Thumb)
         if (other.gameObject.name == rightThumb.name && startDrawing == true)
         {
             drawing = true;
         }
     }
 
+    // User stopped "pinching"
     private void OnTriggerExit(Collider other)
     {
         drawing = false;
